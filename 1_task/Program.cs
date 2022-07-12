a5f54276f2c5d1007bfa31f84f55ca59df55c976
lissa <e-namestnikova@inbox.ru>
2022-07-12 17:19:23 +0700

    add first task

---
+++ 1_task/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве
+int[] numbers = new int[12];
+int count = 0;
+FillArray(numbers);
+PrintArray(numbers);
+
+for (int i = 0; i <= numbers.Length; i++)
+{
+    if (i % 2 == 0)
+    {
+        count += 1;
+    }
+}
+Console.WriteLine($"число положительных чисел равно {count}");
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(99, 1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
